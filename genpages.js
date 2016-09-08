f7ea59db599829ab3660a180f640431c13fa25f2
Ignacio Losiggio <iglosiggio@gmail.com>
2016-09-08 14:51:03 -0300

    Reemplazo los _ y - por espacios

--- genpages.js
+++ genpages.js
@@ -3,10 +3,13 @@ var hbs = require('handlebars');
 var template_raw = fs.readFileSync('template.hbs').toString();
 var sonidos = fs.readdirSync('sonidos');
 var extension = /\.[^.]+$/;
+var espacios = /[_-]/;
 
 var archivos = sonidos.map((sonido) => ({
     archivo: `sonidos/${sonido}`,
-    nombre: sonido.replace(extension, '')
+    nombre: sonido
+        .replace(extension, '')
+        .replace(espacios, ' ')
 }));
 
 var template = hbs.compile(template_raw);
