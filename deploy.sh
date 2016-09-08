c2787b12a77feb956a95c53a7c59a763269fc306
Ignacio Losiggio <iglosiggio@gmail.com>
2016-09-08 15:25:37 -0300

    Comentado el funcionamiento del script de deploy

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
+# Si hay un error o se usa una variable no definida se termina la ejecución
 set -o errexit -o nounset
+# Hash corto de commit
 rev=$(git rev-parse --short HEAD)
 
+# Borro el deploy anterior
 rm -rf deploy
 
+# Instalamos las dependencias
 npm install
 
+# Hacemos la carpeta de deploy y generamos la página
 mkdir deploy
 cp -r sonidos deploy
 node genpages.js > deploy/index.html
 
 (
   cd deploy
+  # Iniciamos un repo vacío con la data del boto
   git init
   git config user.name "Autopush (Travis-CI)"
   git config user.email "iglosiggio@gmail.com"
   git remote add origin "https://${GH_TOKEN}@${GH_REF}"
+  # Pedimos el historial y nos movemos a gh-pages
   git fetch origin
   git reset origin/gh-pages
+  # Agregamos todo lo que haya cambiado y commiteamos
   git add .
   git commit -m "Deploy automático de ${rev}"
+  # Pusheamos a gh-pages
   git push --quiet origin HEAD:gh-pages >/dev/null 2>&1
 )
