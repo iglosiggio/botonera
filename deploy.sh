a30df6cdd6466ae8073425a286665c9c1d04eb93
Ignacio Losiggio <iglosiggio@gmail.com>
2016-09-08 13:07:57 -0300

    Fix typo y dependencias

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,8 @@ node genpages.js > deploy/index.html;
 (
   cd deploy;
   git init;
-  git config.user "Autopush (Travis-CI)";
-  git congit.email "travis@nodemeatspace.com";
+  git config user.name "Autopush (Travis-CI)";
+  git config user.email "iglosiggio@gmail.com";
   git add .
   git commit -m "Deploy automático"
   git push --force --quiet "http://${GH_TOKEN}@${GH_REF}" master:gh-pages >/dev/null 2>&1;
