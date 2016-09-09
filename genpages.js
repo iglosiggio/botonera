3ab9f48aff996f6d20c488372409df2802ac7c1b
Ignacio Losiggio <iglosiggio@gmail.com>
2016-09-09 14:28:54 -0300

    Categoríasgit status

--- genpages.js
+++ genpages.js
@@ -2,18 +2,43 @@ var fs = require('fs');
 var hbs = require('handlebars');
 var template_raw = fs.readFileSync('template.hbs').toString();
 var sonidos = fs.readdirSync('sonidos');
-var extension = /\.[^.]+$/;
-var espacios = /[_-]/g;
+const extension = /\.[^.]+$/;
+const espacios = /[_-]/g;
+const categoria = /^([A-Z]+) /;
+const categoria_default = 'OTROS';
+
+function nombre(sonido, con_categoria) {
+    var nombre = sonido
+        .replace(extension, '')
+        .replace(espacios, ' ');
+
+    return con_categoria
+        ? nombre
+        : nombre.replace(categoria, '');
+}
+
+function categoria_o_default(sonido) {
+    var match = categoria.exec(nombre(sonido, true));
+    return match? match[1] : categoria_default;
+}
 
 var archivos = sonidos.map((sonido) => ({
     archivo: `sonidos/${sonido}`,
-    nombre: sonido
-        .replace(extension, '')
-        .replace(espacios, ' ')
+    categoria: categoria_o_default(sonido),
+    nombre: nombre(sonido)
 }));
 
-var template = hbs.compile(template_raw);
+var categorias = archivos.reduce((categorias, archivo) => {
+    if(categorias[archivo.categoria]) {
+        categorias[archivo.categoria].push(archivo);
+    } else {
+        categorias[archivo.categoria] = [archivo];
+    }
+    return categorias;
+}, {});
 
-var resultado = template({archivos});
+var template = hbs.compile(template_raw);
+debugger;
+var resultado = template({archivos, categorias});
 
 console.log(resultado);
